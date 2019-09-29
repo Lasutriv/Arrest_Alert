1cd3b8b345dd45ffb2732a0aa123a68d4617dbf3
Lasutriv <tannerf1101@yahoo.com>
2019-09-29 15:39:43 -0500

    + Created for specific data analytics on the data gathered and stored.

---
+++ time_analytics.py
@@ -0,0 +1,10 @@
+# Tanner Fry
+# tefnq2@mst.edu
+#
+
+def main():
+    # TODO: Create analytics of the arrests for Missouri, whether it's specific crimes in certain areas or whatever.
+    pass
+
+if __name__ == '__main__':
+    main()
