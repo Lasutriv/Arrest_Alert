7808685e299334fa02fe816a3b94272b0c287f2e
Lasutriv <tannerf1101@yahoo.com>
2019-09-29 15:38:42 -0500

    + Created file reading and set up basic file manipulation for later use. +- Updated arrest data reading and aggregation of said data. +- Updated filtering the data for when it's displayed.

--- scrape_police_reports.py
+++ scrape_police_reports.py
@@ -12,26 +12,53 @@ def main():
     # TODO: Create a fully autonomous routine check that allows for specific
     # CONT: parameters to be passed and if triggered, a notification is then
     # CONT: created.
+
     # Test
-    data_rows = gather_data_from_MO_reports()
-    display_data_rows(data_rows)
+    file_with_data = 'MO_Arrest_Data.txt'
+    start_single_data_check(file_with_data)
+    # start_auto_data_check(file_with_data, 3600)
 
 
 # DATA MACHINE
 
-def start_auto_data_check(update_time: int):
+def start_auto_data_check(file_with_data: str, update_time: int):
     """
     A function that automatically grabs data and compares against the report
     file to see if anything needs to be added.
     Note for update_time: 900 = 15mins, 1800 = 30mins, 3600 = 1hr
 
+    :param file_with_data: the file to compare the gathered data against to see if entries are needed to be added
     :param update_time: the time between data grabs. Should be in seconds.
     :return: none
     """
     # TODO: Finish making below.
     while True:
-        data_rows = gather_data_from_MO_reports()
-        time.sleep(3600)  # Hr break between updates
+        data_rows = gather_data_from_mo_reports(file_with_data)
+        if data_rows == '':
+            exit()
+        else:
+            filtered_data = filter_data_rows(data_rows)
+            # TODO: Check if file exists, if so compare new/old and find last option so we don't push existing content
+            # CONT: to the file.
+            return
+
+        # time.sleep(update_time)  # Hr break between updates
+
+
+def start_single_data_check(file_with_data: str):
+    """
+    A function to grab data and display the arrest results within the area of Missouri.
+
+    :param file_with_data: the file to compare the gathered data against to see if entries are needed to be added
+    :return: none
+    """
+    data_rows = gather_data_from_mo_reports(file_with_data)
+    if data_rows == '':
+        exit()
+    else:
+        add_new_data_to_file(file_with_data, data_rows)
+        # filtered_data = filter_data_rows(data_rows)
+        # display_data_rows(filtered_data)
 
 
 def general_notification_trigger(update_time: int):
@@ -63,39 +90,108 @@ def specific_notification_trigger(update_time: int):
 # DATA COLLECTION
 
 
-def gather_data_from_MO_reports():
+def add_new_data_to_file(file_with_data: str, unfiltered_data: list):
     """
-    Obtains all of the rows and their columns of data from MO's arrest report.
+    A function to check and see if the gathered data should be added to the arrest data file.
 
+    :param file_with_data: the file name where all MO arrest data sits
+    :type file_with_data: str
+    :param unfiltered_data: data from the MO arrest reports website
+    :type unfiltered_data: list
+    :return: none
+    """
+    read_lines = []
+    write_lines = []
+    # Gather data from file
+    with open(file_with_data, 'r') as file:
+        read_lines = file.readlines()
+
+    # Compare file data with scraped data
+    if len(read_lines) is 0:
+        for data in unfiltered_data:
+            write_lines.append(data)
+    else:
+        pass
+
+    for line in write_lines:
+        print('New line: ' + line.replace('\n', ''))
+    # Add data from scraped that wasn't in file
+    # with open(file_with_data, 'w') as file:
+    #     for line in write_lines:
+    #         line = str(line) + '\n'
+    #         file.write(line)
+
+    print('File updated.')
+
+
+def gather_data_from_mo_reports(file_with_data: str):
+    """
+    A function that obtains all of the rows and their columns of data from MO's arrest report.
+
+    :param file_with_data: the file to compare the gathered data against to see if entries are needed to be added
+    :type file_with_data: str
     :return: list of rows from all the data sets
     """
     site = 'https://www.mshp.dps.missouri.gov/HP71/SearchAction'
-    page = requests.get(site)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    arrests_data1 = soup.find_all('td', class_='infoCell')  # 7 cols in a row
-    # TODO: Incorporate the below arrest info under each arrest
-    # arrests_info1 = soup.find_all('td', class_='infoCellPrint')
-    # arrests_info2 = soup.find_all('td', class_='infoCell2Print')
-    # print(arrests_info1[0].find('a').get('href'))
-    arrests_data2 = soup.find_all('td', class_='infoCell2')  # 7 cols in a row
-    arrests_data1_rows = []  # Used to aggregate all the columns of the data set for infoCell
-    arrests_data2_rows = []  # Used to aggregate all the columns of the data set for infoCell2
-    data_sets = [arrests_data1, arrests_data2]
+    arrests_data_rows = []
 
-    arrests_data1_rows = return_data_set_rows(arrests_data1)
-    arrests_data2_rows = return_data_set_rows(arrests_data2)
-    arrests_data_rows = roundrobin(arrests_data1_rows, arrests_data2_rows)
+    try:
+        page = requests.get(site)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        arrests_data1 = soup.find_all('td', class_='infoCell')  # 7 cols in a row
+        # TODO: Incorporate the below arrest info under each arrest
+        # arrests_info1 = soup.find_all('td', class_='infoCellPrint')
+        # arrests_info2 = soup.find_all('td', class_='infoCell2Print')
+        # print(arrests_info1[0].find('a').get('href'))
+        arrests_data2 = soup.find_all('td', class_='infoCell2')  # 7 cols in a row
+        arrests_data1_rows = []  # Used to aggregate all the columns of the data set for infoCell
+        arrests_data2_rows = []  # Used to aggregate all the columns of the data set for infoCell2
+        data_sets = [arrests_data1, arrests_data2]
+
+        arrests_data1_rows = return_data_set_rows(arrests_data1)
+        arrests_data2_rows = return_data_set_rows(arrests_data2)
+        arrests_data_rows = roundrobin(arrests_data1_rows, arrests_data2_rows)
+    except requests.ConnectionError as excep:
+        print('Connection error: ' + str(excep) + '\n\nReading from ' + file_with_data + ':')
+        read_info = []
+
+        try:
+            with open(file_with_data, 'r') as file:
+                read_info = file.read()
+            if read_info != '':
+                return read_info
+            else:
+                print('Nothing here to parse. Exiting.')
+                return ''
+        except FileNotFoundError:
+            file_opened = open(file_with_data, 'w')
+            file_opened.close()
 
     return arrests_data_rows
 
+
 # DATA DISPLAY
 
-
-def display_data_rows(data_rows: list, filter_age=None, filter_city=None, filter_date=None, filter_time=None, filter_county=None, filter_troop_id=None):
+def display_data_rows(filtered_data: list):
     """
-    Function used to display aggregated data from the Missouri Police Reports
+    Function used to display aggregated data from the Missouri Police Reports.
+
+    :param filtered_data: collection of data rows, each row representing an arrest
+    :return: none
+    """
+    column_names = ['Age', 'City/State', 'Date', 'Time', 'County', 'Troop ID']
+    print(pd.DataFrame(data=filtered_data, index=None, columns=column_names))
+
+# DATA FILTER
+
+
+def filter_data_rows(data_rows: list, filter_age=None, filter_city=None, filter_date=None, filter_time=None, filter_county=None, filter_troop_id=None):
+    """
+    Function used to filter  aggregated data from the Missouri Police Reports
     website in a pandas dataframe view. The data can be filtered with many
     different variations.
+
+    Inserted data looks like:
 
     Note: filter_age uses this syntax: filter_age='begin_int-end_int'
 
@@ -106,10 +202,9 @@ def display_data_rows(data_rows: list, filter_age=None, filter_city=None, filter
     :param filter_time: a filter parameter to see arrests from a specific time
     :param filter_county: a filter parameter to see arrests from a specific county
     :param filter_troop_id: a filter parameter to see arrests from a specific highway patrol troop
-    :return: none
+    :return: list of data rows, each row representing an arrest
     """
     arrest_age, arrest_city_state, arrest_date, arrest_time, arrest_county, arrest_troop_id = '', '', '', '', '', ''
-    column_names = ['Age', 'City/State', 'Date', 'Time', 'County', 'Troop ID']
     data_filtered = []
     for row in data_rows:
         # Apply filters
@@ -160,7 +255,7 @@ def display_data_rows(data_rows: list, filter_age=None, filter_city=None, filter
         # Reset variables
         arrest_age, arrest_city_state, arrest_date, arrest_time, arrest_county, arrest_troop_id = '', '', '', '', '', ''
 
-    print(pd.DataFrame(data=data_filtered, index=None, columns=column_names))
+    return data_filtered
 
 # TOOLS
 
